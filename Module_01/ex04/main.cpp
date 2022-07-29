0a72ece6cf1ba8d162178a19b8b5b8dcd8aa1a94
Anass Mounadi <mounadi2015@gmail.com>
2022-07-29 08:05:44 +0100

    add ex014

--- Module_01/ex04/main.cpp
+++ Module_01/ex04/main.cpp
@@ -9,28 +9,21 @@ int main (int ac, char **av)
     if (ac == 4)
     {
         int min = 0;
-        int max = 0;
         std::string str;
         file_o.open(av[1] + name);
         file_i.open(av[1]);
-        std::string replace = av[3]; 
+        std::string find = av[2];
         while(getline(file_i, str))
         {
-            if (str.find(av[2]) != - 1)
+            while(((int )str.find(av[2]) != - 1))
             {
-                min  = str.find_first_of(av[2]);
-                max  = str.find_last_of(av[2]);
-                int i = 0;
-                for(; min <= max ; min++)
-                    str[min] = replace[i++];
-                if (replace[i] != '\0')
-                    str.insert(i , replace.substr(i));
-            }    
+                min = str.find(av[2]);
+                str.erase(min, find.length());
+                str.insert(min,av[3]);
+            }  
             file_o << str << std::endl;
-           
         }
     }
     else 
-        std::cout << "\t * wrong arguments *" << std::endl; 
-    
+        std::cout << "\t * wrong arguments *" << std::endl;
 }
